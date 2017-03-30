d3c032a57e61abb03f17bdc038675c5f6ba28c1d
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-03-31 01:08:27 +0530

    Django Admin Added.

--- sherlocked/admin.py
+++ sherlocked/admin.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from sherlocked.models import *
 
+
+class UserDetailAdmin(admin.ModelAdmin):
+    list_display = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
+    search_list = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
+    list_filter = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
+
 admin.site.register(Question)
-admin.site.register(UserDetail)
-
-# class AppnameAdmin(admin.ModelAdmin):
-#     list_display = ('')
-
-# admin.site.register(Questions,AppnameAdmin)
+admin.site.register(UserDetail, UserDetailAdmin)
