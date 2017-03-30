d0ab6c911a426417261eb0c1511429ba33f0f45b
RishabhJain2018 <rishabhjain2018@gmail.com>
2017-03-31 01:11:42 +0530

    Minor chnages in admin

--- sherlocked/admin.py
+++ sherlocked/admin.py
@@ -4,7 +4,7 @@ from sherlocked.models import *
 
 class UserDetailAdmin(admin.ModelAdmin):
     list_display = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
-    search_list = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
+    search_fields = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
     list_filter = ('Zealid', 'CurrentQuestionNo', 'LastSolvedAt', 'college', 'phno',)
 
 admin.site.register(Question)
